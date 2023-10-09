9d144ea982a6dd6f45d42fec546416c804240317
rume edgar <rumeedgar@icloud.com>
2023-10-09 12:37:11 +0100

    load images

--- rec_engine.py
+++ rec_engine.py
@@ -12,3 +12,14 @@ class RecEngine:
 
         # Resize frame for a faster speed
         self.frame_resizing = 0.25
+
+    def load_encoding_images(self, images_path):
+        """
+        Load encoding images from path
+        :param images_path:
+        :return:
+        """
+        # Load Images
+        images_path = glob.glob(os.path.join(images_path, "*.*"))
+
+        print("{} encoding images found.".format(len(images_path)))    
