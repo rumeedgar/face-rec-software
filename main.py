a730721b8158abb00eeba46c29c695d679685cc1
rume edgar <rumeedgar@icloud.com>
2023-10-09 12:28:53 +0100

    wrote code to encode faces from a folder, load camera and detect faces

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2
+from rec_engine import RecEngine
+import time
+
+# Encode faces from a folder
+fr = RecEngine()
+fr.load_encoding_images("images/")
+
+# Load Camera
+cap = cv2.VideoCapture(0)
+time.sleep(2)
+
+while True:
+    ret, frame = cap.read()
+
+    # Detect Faces
+    face_locations, face_names = fr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
+        cv2.putText(frame, name, (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 4)
+
+    cv2.imshow("Face Recognition", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
